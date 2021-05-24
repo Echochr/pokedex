d26d26d72ecee10570420b0e39642a1cd017dc0b
Chris Ho <chrisho0418@gmail.com>
2021-05-24 15:31:47 +0800

    Refactor createPokemonCard function calling

--- app.js
+++ app.js
@@ -58,12 +58,18 @@ async function createPokemonCard(startID, endID, genNum) {
     }
 }
 
-createPokemonCard(1, 151, 1);
-createPokemonCard(152, 251, 2);
-createPokemonCard(252, 386, 3);
-createPokemonCard(387, 493, 4);
-createPokemonCard(494, 649, 5);
-createPokemonCard(650, 721, 6);
-createPokemonCard(722, 809, 7);
-createPokemonCard(810, 898, 8);
+const generationsIndex = [
+    null,
+    { startID: 1, endID: 151 },
+    { startID: 152, endID: 251 },
+    { startID: 252, endID: 386 },
+    { startID: 387, endID: 493 },
+    { startID: 494, endID: 649 },
+    { startID: 650, endID: 721 },
+    { startID: 722, endID: 809 },
+    { startID: 810, endID: 898 }
+]
 
+for (let i = 1; i <= 8; i++) {
+    createPokemonCard(generationsIndex[i].startID, generationsIndex[i].endID, i)
+}
